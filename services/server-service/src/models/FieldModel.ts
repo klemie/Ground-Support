cdc0f86ee42cb7d895a033c8a653f8593685636c
klemie <lemieuxkristopher@gmail.com>
2023-01-17 18:18:50 -0800

    entities

---
+++ services/server-service/src/models/FieldModel.ts
@@ -0,0 +1,30 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+export interface IField {
+    Name: String;
+    Range: Array<Number>
+};
+
+export interface IGenericModel extends IField, Document { };
+
+const FieldSchema: Schema = new Schema(
+    {
+        Name: {
+            type: String,
+            required: true
+        },
+        Range: {
+            type: Array<Number>,
+            required: true,
+            validator: (r: Number) => {
+
+            }
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: true
+    }
+);
+
+export default mongoose.model<IField>('Generic', FieldSchema);
