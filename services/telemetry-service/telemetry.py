eb95c61fe5a291cd93a272c28bc049e40b29f722
JackCotter <79673714+JackCotter@users.noreply.github.com>
2023-01-20 16:37:27 -0800

    working it out

--- services/telemetry-service/telemetry.py
+++ services/telemetry-service/telemetry.py
@@ -4,4 +4,5 @@ import sys
 import shutil
 from subprocess import Popen, PIPE
 
-a = Popen(["./decode.sh", 19, 19, 19])
+a = Popen(["rtl-sdr/rtl_fm", "-f", "194.3"], stdout=PIPE, stderr=PIPE)
+b = Popen(["./direwolf-1.6.0-413855e_x86_64/direwolf"], stdin=a.stdout)
