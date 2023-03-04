f7eb8084d2f0e78503ce5e15836402a39687e083
UVR-Admin <uvicrocketry@gmail.com>
2023-03-03 16:04:27 -0800

    module merge

--- client/src/components/module.tsx
+++ client/src/components/module.tsx
@@ -1,67 +1,137 @@
-import React, { ReactText } from "react";
-import { Card, Chip, TextField,
-    CardContent, 
-    CardActionArea, 
-    Typography, 
-    CardHeader
-} from "@mui/material";
-import { Stack } from "@mui/system";
+import React, { useEffect, useState } from 'react';
+import { 
+	Card, 
+	Chip, 
+	TextField, 
+	CardContent, 
+	Divider, 
+	CardHeader, 
+	Tooltip
+} from '@mui/material';
+import { Stack } from '@mui/system';
+import _ from 'lodash';
 
+const CHUNK_SIZE = 3;
 
-const statusMap = new Map<String, String> ([
-    ["Inactive", "FCB701"],
-    ["Active", "#65C464"],
-    ["sus", "#FFF"]
-])
+const statusMap = new Map<string, string>([
+	['Inactive', '#FCB701'],
+	['Active', '#65C464'],
+	['Default', 'grey'],
+	['Failed', '#C6232C']
+]);
+
+export interface Field {
+	module: string;
+	fieldName: string;
+	fieldRange: [number, number];
+	fieldValue: number;
+}
 
 interface ModuleProps {
-    value?: Object,
-    Title?: String,
-    status?: String
+	title: string;
+	fields: Array<Field>;
 }
 
-const Module: React.FC<ModuleProps> = ( props: ModuleProps) => {
-    const [value, setValue] = React.useState(props.value);
-    const [status, setStatus] = React.useState(props.status);
+const computeStatuses = (fields?: Array<Field>) => {
+	let statuses: [string] = [""];
+	let status: string = 'Inactive';
+	fields?.forEach((field) => {
+		const max = field.fieldRange[0];
+		const min = field.fieldRange[1];
+		if (_.inRange(field.fieldValue, min, max)) {
+			status = 'Active';
+		} else if (field.fieldValue === 0) {
+			status = 'Inactive';
+		} else {
+			status = 'Failed';
+		}
+		statuses.push(status);
+	});
+	return statuses;
+};
 
-    const updateStatus = () => {
-        setStatus('random string');
-    };
+const Module: React.FC<ModuleProps> = (props: ModuleProps) => {
+	const [statusColor, setStatusColor] = useState(statusMap.get('Inactive')); //TODO: change to state array for all fields + module status
+	const [status, setStatus] = useState('Inactive');
 
-    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setValue(event.target.value);
-    };
-    return (
-        <>
-            <Card
-                variant = "outlined"
-                sx = {{ minWidth: 363 }}
-            >
-                <CardContent>
-                    <CardHeader title = {props.Title || "Module" }>
+	useEffect(() => {
+		const statuses = computeStatuses(props.fields);
+		statuses.forEach((st) => {
+			setStatus(st);
+			setStatusColor(statusMap.get(st));
+		}); // currently only the final field controls the whole status of the module
+	}, [props.fields]);
 
-                    </CardHeader>
-                    <Stack direction="column">
-                    <TextField
-                        id="value-text-feild"
-                        label = "Jack's Typing Box (not Really)"
-                        value={ value }
-                        defaultValue=':) hey. hows it going'
-                        onChange={handleChange}
-                    >
-                    </TextField>
-                    </Stack>
-                <CardActionArea>
-                    <Chip
-                        color='primary'
-                        label={ status }
-                    />
+	const fieldsMatrix = _.chunk(props.fields, CHUNK_SIZE);
+	
+	return (
+		<>
+			<Card variant="outlined">
+				<CardHeader 
+					title={props.title || 'Default'} 
+					titleTypographyProps={{ variant: 'subtitle1' }} 
+					sx={{ padding: 2 }} 
+				/>
+				<CardContent sx={{ paddingBlockStart: 0, paddingBlockEnd: 0 }}>
 
-                </CardActionArea>
-                </CardContent>
-            </Card>
-        </>
-    )
-}
+					<Divider variant="fullWidth" sx={{ mb: 2 }} />
+
+					<Stack 
+						spacing={3} 
+						direction="row" 
+						justifyContent="center"
+					>
+						{fieldsMatrix.map((cols: Array<any>) => {
+							return ( 
+								<Stack 
+									spacing={3} 
+									direction="column" 
+									alignItems="center"
+								> 
+								{
+									cols.map((field, i) => {
+										return (
+											<Tooltip 
+												key={i} 
+												title={field.fieldName} 
+												placement="bottom"
+											>
+												<TextField
+													key={i} 
+													id="value-text-field"
+													size="small" 
+													label={field.fieldName} 
+													value={field.fieldValue} 
+													defaultValue="NaNa" 
+													fullWidth
+													sx= {{
+														"min-width": 100,
+														"max-width": 150,
+														input: {
+															textAlign: "center"
+														}
+													}}
+												/>
+											</Tooltip>
+										)
+									})
+								}
+								</Stack>
+							)
+						})}
+					</Stack>
+
+					<Divider variant="fullWidth" sx={{ my: 2 }} />
+
+					<Chip 
+						color="primary" 
+						style={{ backgroundColor: String(statusColor) }} 
+						label={status} 
+					/>
+				</CardContent>
+			</Card>
+		</>
+	);
+};
 
 export default Module;
