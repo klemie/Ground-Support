c7714a39ac2c0b01aacf50028f640bca0b7499ae
klemie <lemieuxkristopher@gmail.com>
2023-06-14 15:12:49 -0700

    Setup Frontend socket provider for the gateway

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './root/App';
 import reportWebVitals from './reportWebVitals';
+import { SocketGateway } from './utils/socket-context';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+	document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+<React.StrictMode>
+	<SocketGateway>
+		<App />
+	</SocketGateway>
+</React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ client/src/utils/socket-context.tsx
@@ -0,0 +1,78 @@
+import {
+    createContext,
+    PropsWithChildren,
+    useContext,
+    useEffect,
+    useState
+} from 'react';
+import { socket } from './socket-port';
+
+export interface SocketContext {
+    logs: string[];
+    aprsPacket: string | null;
+    loRaPacket: string | null;
+    setAprsFrequency: (frequency: number) => void;
+    setLoRaFrequency: (frequency: number) => void;
+}
+
+export const Context = createContext<SocketContext>({
+    logs: [],
+    aprsPacket: null,
+    loRaPacket: null,
+    setAprsFrequency: (frequency: number) => {},
+    setLoRaFrequency: (frequency: number) => {}
+});
+
+export const SocketGateway = ({ children }: PropsWithChildren<any>) => {
+    const [logs, setLogs] = useState<string[]>([]);
+    const [aprsPacket, setAprsPacket] = useState<string | null>(null);
+    const [loRaPacket, setLoRaPacket] = useState<string | null>(null);
+    
+    const setAprsFrequency = (frequency: number) => {
+        socket.emit('set_aprs_frequency', { frequency });
+    };
+
+    const setLoRaFrequency = (frequency: number) => {
+        socket.emit('set_loRa_frequency', { frequency });
+    };
+
+    useEffect(() => {
+        socket.on('connect', () => {
+            console.log('Connected to Telemetry')
+        });
+
+        socket.on('loRa_packet', (packet: string) => {
+            setLoRaPacket(packet);
+        });
+
+        socket.on('aprs_packet', (packet: string) => {
+            setAprsPacket(packet);
+        });
+        
+        socket.on('logs', (data: any) => {
+            setLogs((prev) => [...prev, JSON.stringify(data)]);
+        });
+
+        return () => {
+            socket.disconnect();
+        }
+    }, []);
+
+    return (
+        <Context.Provider
+            value={{
+                logs,
+                aprsPacket,
+                loRaPacket,
+                setAprsFrequency,
+                setLoRaFrequency
+            }}
+        >
+            {children}
+        </Context.Provider>
+    );
+}
+
+export const useSocketContext = () => {
+    return useContext<SocketContext>(Context);
+};
