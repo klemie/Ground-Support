a17c33a103e1407ebbc11e0d8d627b30ee590657
klemie <lemieuxkristopher@gmail.com>
2023-06-14 16:15:38 -0700

    PR suggestions 👉👈 & interfaces instead of types

--- client/src/utils/entities.ts
+++ client/src/utils/entities.ts
@@ -1,38 +1,38 @@
 enum TelemetrySource {
     lora = "LORA",
     aprs = "APRS"
-};
-
-export type Field = {
-    Name: string,
-    Range: [number],
-    Units: string,
-    TelemetryId: number
-};
-
-export type Module = {
-    Name: string,
-    FieldGroups: [Field]
-};
-
-export type DataConfig = {
-    Modules: [Module]
-};
-
-type Coordinates = {
-    Latitude: number,
-    Longitude: number
 }
 
-export type Mission = {
+export interface IField {
     Name: string,
-    IsTest: boolean,
-    Date: Date,
-    Coordinates: Coordinates,
-    LaunchAltitude: Coordinates,
-    Components: [],
-    Published: boolean
-};
+    Range: number[],
+    Units: string,
+    TelemetryId: number
+}
+
+export interface IModule {
+    Name: string;
+    FieldGroups: IField[];
+}
+
+export interface IDataConfig {
+    Modules: IModule[];
+}
+
+interface ICoordinates {
+    Latitude: number;
+    Longitude: number;
+}
+
+export interface IMission {
+    Name: string;
+    IsTest: boolean
+    Date: Date;
+    Coordinates: ICoordinates;
+    LaunchAltitude: Number;
+    Components: [];
+    Published: boolean;
+}
 
 enum MotorType {
     solid = "Solid",
@@ -40,20 +40,39 @@ enum MotorType {
     hybrid = "Hybrid"
 };
 
-export type Rocket = {
-    Name: string,
-    Missions: [],
-    Components: [],
-    Mass: number,
-    Height: number,
-    Class: string,
-    MotorType: string,
-    Motor: string
-};
+export interface IRocket{
+    Name: string;
+    Missions: [];
+    Components: [];
+    Mass: number;
+    Height: number;
+    Class: string;
+    MotorType: MotorType;
+    Motor: string;
+}
 
-export type Component = {
-    Name: string,
-    telemetrySource: TelemetrySource,
+export interface IComponent {
+    Name: string;
+    TelemetrySource: TelemetrySource;
     Details: string,
-    DataConfig: DataConfig
-};
+    DataConfig: IDataConfig
+}
+export interface IFieldGroup {
+    Name: String;
+    Fields: IField[];
+}
+
+interface IDataPoint {
+    Value: number,
+    timeStamp: Date
+}
+
+export interface IFieldData {
+    Name: string;
+    Range: number[];
+    Units: string;
+    ParentModuleName: string;
+    ParentFieldGroupName: string;
+    TelemetryId: Buffer;
+    Data: IDataPoint[];
+}
