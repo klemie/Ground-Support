5bb26b1b126c80dcc62d426f9e211ee48f4ed347
klemie <lemieuxkristopher@gmail.com>
2023-06-29 13:16:27 -0700

    MissionConfig Popup update

--- client/src/components/MissionConfig.tsx
+++ client/src/components/MissionConfig.tsx
@@ -1,20 +1,56 @@
-import React, { useState } from "react";
-import { Button, Dialog, TextField, Stack, DialogContent, Typography, Checkbox, FormControlLabel, InputAdornment, Tooltip, IconButton  } from "@mui/material";
-import { Upload, Info } from "@mui/icons-material"; 
+import React, { useCallback, useEffect, useState } from "react";
+import { Button, Dialog, TextField, Stack, DialogContent, Typography, Checkbox, FormControlLabel, InputAdornment, Tooltip, IconButton, DialogActions, Select, MenuItem, OutlinedInput, Box, Chip, FormControl, InputLabel  } from "@mui/material";
+import { IRocket, IComponent } from "../utils/entities";
+
+import axios from "axios";
+
+interface RocketDetails extends IRocket {
+    Id?: string;
+}
 
 interface MissionConfigProps {
     missionId?: string;
+    rocket?: RocketDetails;
     isOpen: boolean;
     onClose: () => void;
     onSave: () => void;
 }
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+
+const MenuProps = {
+	PaperProps: {
+		style: {
+			maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+			width: 250
+		}
+	}
+};
+
 const MissionConfig: React.FC<MissionConfigProps> = (props: MissionConfigProps) => {
+    const { rocket } = props;
     const [missionName, setMissionName] = useState<string>();
     const [latitude, setLatitude] = useState<string>();
     const [longitude, setLongitude] = useState<string>();
     const [launchDate, setLaunchDate] = useState<Date>();
     const [altitude, setAltitude] = useState<string>();
+    const [selectedComponents, setSelectedComponents] = useState<string[]>([]);
+    const [components, setComponents] = useState<string[]>([]);
+
+    const getComponents = useCallback(async () => {
+        try {
+            if (rocket) {
+                rocket.Components.map( async (componentId: string) => {
+                    const response = await axios.get(`http://127.0.0.1:9090/component/${componentId}`);
+                    const data = response.data.result as IComponent;
+                    setComponents((prev) => [...prev, data.Name]);
+                });
+            }
+        } catch (error) {
+            console.error(error);
+        } 
+    }, []);
 
     const handleChange = (e: any, setState: Function) => {
 		// if (!editMode) {
@@ -22,6 +58,12 @@ const MissionConfig: React.FC<MissionConfigProps> = (props: MissionConfigProps) 
 		// }
 		setState(e.target.value as string);
 	};
+
+    useEffect(() => {
+        // clear the components array
+        setComponents([]);
+        getComponents();
+    }, []);
 
     return(
         <Dialog open={props.isOpen} fullWidth>
@@ -95,28 +137,55 @@ const MissionConfig: React.FC<MissionConfigProps> = (props: MissionConfigProps) 
                         value={altitude}
                         onChange={(e) => handleChange(e, setAltitude)}
                     />
-
-                    <Stack direction="row" spacing={2} alignItems="center">
-                        <Button fullWidth={true} variant={"contained"} component="label"
-                            startIcon={<Upload/>}
+                    <FormControl fullWidth variant="filled" required>
+						<InputLabel id="component-source-label">Configure Components</InputLabel>
+                        <Select
+                            id="component-source"
+                            variant="filled"
+                            multiple
+                            fullWidth
+                            value={selectedComponents}
+                            onChange={(e) => handleChange(e, setSelectedComponents)}
+                            input={<OutlinedInput id="select-multiple-chip" label="Chip" />}
+                            // helperText="Configure components for this mission"
+                            renderValue={(selected: string[]) => (
+                                <Box
+                                    sx={{
+                                        display: 'flex',
+                                        flexWrap: 'wrap',
+                                        gap: 0.5,
+                                        transform: 'translateY(20%)'
+                                    }}
+                                >
+                                    {selected.map((value: string) => (
+                                        <Chip key={value} label={value} />
+                                    ))}
+                                </Box>
+                            )}
+                            sx={{
+                                '& .MuiOutlinedInput-root.Mui-focused .MuiOutlinedInput-notchedOutline': {
+                                    border: 'none !important'
+                                }
+                            }}
+                            MenuProps={MenuProps}
+                            labelId="component-source-label"
+                            label="Enable Components"
                         >
-                            Data Configuration 
-                            <input hidden type="file" />
-                        </Button>
-                        <IconButton>
-                            <Info />
-                        </IconButton>
-                    </Stack>
-                    <Stack direction="row" spacing={2} alignItems="center">
-
-                        <Button variant={"contained"} sx={{ minWidth: "48.5%"}} onClick={props.onSave}>Save</Button>
-
-                        <Button variant={"contained"} color={"error"} sx={{ minWidth: "48.5%"}} onClick={props.onClose}>Cancel</Button>
-
-                    </Stack>
-
+                            {
+                            components.map((component: string, idx) => (
+                                <MenuItem key={idx} value={component}>
+                                    {component}
+                                </MenuItem>
+                            ))
+                            }
+                        </Select>
+                    </FormControl>
                 </Stack>
             </DialogContent>
+            <DialogActions>
+                <Button color={"error"} onClick={props.onClose}>Cancel</Button>
+                <Button onClick={props.onSave}>Save</Button>
+            </DialogActions>
         </Dialog>
     );
 }
