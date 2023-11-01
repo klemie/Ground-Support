75259efe6ca523f2481aafd3df925444640181f2
Matt Stephenson <matthewstephenson39@gmail.com>
2023-11-01 13:35:23 -0700

    use root env file for docker compose

--- services/server/src/config/config.ts
+++ services/server/src/config/config.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 
+// Note: if not running in a Docker container, copy .env file from root project directory into directory containing this file 
 dotenv.config();
 
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
@@ -11,9 +12,9 @@ const Local_DB = (MONGO_LOCAL == 'True');
 
 const MONGO_URL_REMOTE = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@groundsupport.${MONGO_DB_STRING}.mongodb.net/`;
 const MONGO_URL_LOCAL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@db:27017/${MONGO_DB_STRING}?authSource=admin`;
-const MONGO_URL = Local_DB ? MONGO_URL_LOCAL : MONGO_URL_REMOTE
+const MONGO_URL = Local_DB ? MONGO_URL_LOCAL : MONGO_URL_REMOTE;
 
-const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
+const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 9090;
 
 export const config = {
     mongo: {
