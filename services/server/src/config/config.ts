d2e04df4ff16d080528f3b42480c5d30a8160ee3
mas393 <matthewstephenson39@gmail.com>
2023-11-01 09:51:47 -0700

    working local container with hardcoded env vars

--- services/server/src/config/config.ts
+++ services/server/src/config/config.ts
@@ -5,12 +5,14 @@ dotenv.config();
 const MONGO_USERNAME = process.env.MONGO_USERNAME || '';
 const MONGO_PASSWORD = process.env.MONGO_PASSWORD || '';
 const MONGO_DB_STRING = process.env.MONGO_DB_STRING || '';
+const MONGO_LOCAL = process.env.MONOG_LOCAL || 'True';
 
-console.log(process.env.MONGO_PASSWORD)
+const Local_DB = (MONGO_LOCAL == 'True');
 
-console.log(`DataBase Username: ${MONGO_USERNAME}\nDataBase Password: ${MONGO_PASSWORD}`);
+const MONGO_URL_REMOTE = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@groundsupport.${MONGO_DB_STRING}.mongodb.net/`;
+const MONGO_URL_LOCAL = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@db:27017/${MONGO_DB_STRING}?authSource=admin`;
+const MONGO_URL = Local_DB ? MONGO_URL_LOCAL : MONGO_URL_REMOTE
 
-const MONGO_URL = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@groundsupport.${MONGO_DB_STRING}.mongodb.net/`;
 const SERVER_PORT = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 1337;
 
 export const config = {
