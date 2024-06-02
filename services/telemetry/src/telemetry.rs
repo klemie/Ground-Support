0b5500bd2f73d08e12727004555f8e020abad11c
Jack Cotter <jackwcotter101@gmail.com>
2024-06-01 19:24:12 -0700

    sending in batch

--- services/telemetry/src/telemetry.rs
+++ services/telemetry/src/telemetry.rs
@@ -59,7 +59,7 @@ pub async fn start_telemetry(state_ref: Arc<Mutex<State>>) -> io::Result<()> {
     println!("Starting telemetry on {}", frequency);
 
     let mut decode_aprs = Command::new("sh")
-        .arg("./tools/decode.sh")
+        .arg("./tools/decode_test.sh")
         // .arg(frequency.to_string())
         .stdout(Stdio::piped())
         .spawn()
@@ -71,6 +71,7 @@ pub async fn start_telemetry(state_ref: Arc<Mutex<State>>) -> io::Result<()> {
     loop {
         // read() blocks until data is available. This makes this not polling.
         let mut b = child_out.read(&mut readbuf);
+        // println!("{:?}",b);
         extract_packets(Arc::clone(&state_ref), readbuf.clone()).await;
     }
     Ok(())
---
+++ services/telemetry/tools/decode_test.sh
@@ -0,0 +1,1 @@
+python3 ./tools/test_data.py
